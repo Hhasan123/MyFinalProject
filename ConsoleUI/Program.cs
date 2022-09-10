8a4cc972f83ab6bc44f927c7460efbe4b50f9172
Hhasan123 <hy907544@gmail.com>
2022-09-10 10:33:57 +0300

    10922

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InProductMemory;
 using System;
 
@@ -9,10 +10,20 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAll())
             {
-                Console.WriteLine(product.ProductName );
+                Console.WriteLine(product.ProductName);
             }
         }
     }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
@@ -11,61 +12,11 @@ using System.Text;
 namespace DataAccess.Concrete
 {
     //NuGet
-    public class EfProductDal : IProductDal
+    public class EfProductDal :EfEntityRepositoryBase<Product,NorthwindContext> ,IProductDal
     {
-        public void Add(Product entity)
-        {
-            //using= Disposable pattern implementation of c#
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEtity = context.Entry(entity); //referansı yakala
-                addedEtity.State = EntityState.Added;   // nesneyi ekle
-                context.SaveChanges();                  // ve kaydet
-
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            //using= Disposable pattern implementation of c#
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var deleteEtity = context.Entry(entity); //referansı yakala
-                deleteEtity.State = EntityState.Deleted;   // nesneyi ekle
-                context.SaveChanges();                  // ve kaydet
-
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            //using= Disposable pattern implementation of c#
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var updatedEtity = context.Entry(entity); //referansı yakala
-                updatedEtity.State = EntityState.Modified;   // nesneyi ekle
-                context.SaveChanges();                  // ve kaydet
-
-            }
-        }
+        //EfEntityRepositoryBase class ında IProductDal ın istediği metodlar zaten olduğundan
+        //EfProductDal EfEntityRepository i inherit ettiğinde IProductDal ın istediği metodlar
+        //EfProductDal içerisinde oluşmutur.
+        
     }
 }
